8ed7327186eb4311d55274e8cf905202ca6d8687
pessimistinc <55163229+pessimistinc@users.noreply.github.com>
2019-10-24 17:13:26 -0500

    i did things

--- src/main/java/passmanager/controller/AppController.java
+++ src/main/java/passmanager/controller/AppController.java
@@ -1,5 +1,21 @@
 package passmanager.controller;
 
-public class AppController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
+public class AppController {
+	
+	
+	@FXML
+	public void processLogin(ActionEvent event) {
+	
+		String value = ((Button)event.getSource()).getText();
+		
+		if(value.equals("New User")) System.out.println("you need to make a new username");
+		
+		else if(value.equals("Unlock")) System.out.println("you clicked unlock");
+		
+	}
+	
 }
