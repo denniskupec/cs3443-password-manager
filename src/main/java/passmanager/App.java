cd4c5770be40c00fcfcfc39619d15ed5acb7fd10
Dennis Kupec <denniskupec@gmail.com>
2019-10-28 16:11:57 -0500

    Using a logging interface is not going to work, since default methods are public and overridable.

--- src/main/java/passmanager/App.java
+++ src/main/java/passmanager/App.java
@@ -1,45 +1,62 @@
 package passmanager;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import passmanager.controller.AppController;
 
 public class App extends Application {
 
-	private final static Logger log = Util.getLogger(App.class);
-//	private AppController app = new AppController();
-	private int sceneMarker;
-	private String login = "/layout/login.fxml";
-	private String newUser = "/layout/newUser.fxml";
-	private String current = "/layout/login.fxml";
-	
-
-	@Override
-	public void start(Stage primaryStage) throws Exception{
-		
-			Parent root = FXMLLoader.load(getClass().getResource(current));
-
-			Scene scene = new Scene(root, 1133, 700);
-				  scene.getStylesheets().add(getClass().getResource("/style/app.css").toExternalForm());
-				  
-				  
-			primaryStage.setTitle("Password Manager");
-			primaryStage.setScene(scene);
-			primaryStage.setResizable(false);
-			primaryStage.show();
-		
-	}
-
+	private final static Logger Log = Util.getLogger(App.class);
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		Parent root = FXMLLoader.load(getClass().getResource("/layout/login.fxml"));
+
+		Scene scene = new Scene(root, 1133, 700);
+			scene.getStylesheets().add(getClass().getResource("/style/app.css").toExternalForm());
+			  
+		primaryStage.setTitle("Password Manager");
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+
+		Log.info("Data path: " + App.getStoragePath().getPath());
+	}
+
+
+	/**
+	 * In this application, data is stored in the same directory as the jarfile. 
+	 * @return URL
+	 */
+	public static URL getStoragePath() {
+		return App.class.getProtectionDomain().getCodeSource().getLocation();
+	}
+	
+	/**
+	 * Returns the path to a specific file in the storage directory.
+	 * @param String 	name of the file
+	 * @return URL
+	 * @throws MalformedURLException 
+	 */
+	public static URL getStoragePath(String filename)  {
+		URL u = null;
+		try {
+			u = new URL(App.getStoragePath(), filename);
+		}
+		catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		return u;
 	}
 
 }
