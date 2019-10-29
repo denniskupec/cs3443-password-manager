a4671971f6a0c467739eb597bccbd2f09505d65c
Dennis Kupec <denniskupec@gmail.com>
2019-10-28 22:54:03 -0500

    WIP: database access and initialization

--- src/main/java/passmanager/Database.java
+++ src/main/java/passmanager/Database.java
@@ -2,25 +2,61 @@ package passmanager;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.logging.Logger;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
-import java.io.FileNotFoundException;
+import java.nio.file.Files;
 
 public class Database {
 
+	private final static Logger Log = Util.getLogger(Database.class);
+	
 	/**
-	 * handle to the 
+	 * database file
+	 */
+	private File dbFile;
+	
+	/*
+	 * database connection
 	 */
 	private Connection conn;
+	
 
-	Database() throws FileNotFoundException {
-		App.class.getResource("/storage.");
+	Database() throws SQLException {
+		try {
+			dbFile = new File(App.getStoragePath("storage.sqlite3").toURI());
+		}
+		catch (URISyntaxException e) {
+			e.printStackTrace(); // probably need a more graceful way of handling this
+		}
+		
+		/* First time running the application, database deleted, etc. 
+		 * This copies the database template file. */
+		if (!dbFile.exists()) {
+			Log.info("Initializing database.");
+			
+			try {
+				Files.copy(App.getResource("storage.sqlite3"), dbFile.toPath());
+			} 
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		conn = DriverManager.getConnection("jdbc:sqlite:" + dbFile.getPath());
 	}
 
+	/**
+	 * Closes the database connection.
+	 * @throws SQLException
+	 */
 	public void close() throws SQLException {
-		this.conn.close();
+		if (conn != null) {
+			conn.close();
+		}
 	}
 
 }
