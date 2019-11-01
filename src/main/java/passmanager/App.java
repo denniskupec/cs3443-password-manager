5bc75af82df2ad273a66c8932f0b6104966aec37
Abit Gurung <gurungabit@gmail.com>
2019-11-01 03:14:01 -0500

    made Login primary, changed accounts view

--- src/main/java/passmanager/App.java
+++ src/main/java/passmanager/App.java
@@ -20,7 +20,7 @@ public class App extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-		Parent root = FXMLLoader.load(getClass().getResource("/layout/signup.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("/layout/login.fxml"));
 		//Scene scene = new Scene(root, 1133, 700);
 		Scene scene = new Scene(root);
 		scene.getStylesheets().add(getClass().getResource("/style/app.css").toExternalForm());
