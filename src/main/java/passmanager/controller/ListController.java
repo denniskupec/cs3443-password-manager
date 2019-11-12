fa8218d36f685f6931b2a35daf9b192c4374b273
pessimistinc <55163229+pessimistinc@users.noreply.github.com>
2019-11-12 13:48:07 -0600

    implemented new password entry in list

--- src/main/java/passmanager/controller/ListController.java
+++ src/main/java/passmanager/controller/ListController.java
@@ -1,5 +1,37 @@
 package passmanager.controller;
 
+import java.util.logging.Logger;
+
+import javafx.event.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import passmanager.Util;
+
 public class ListController extends BaseController {
 
+	private final static Logger Log = Util.getLogger(ListController.class);
+	
+	@FXML TextField website;
+	@FXML TextField username;
+	@FXML TextField password;
+	@FXML TitledPane newPassword;
+	@FXML MenuItem choiceNew;
+	@FXML Button save;
+	
+	public void onNewEntry(Event event) {
+		
+		newPassword.setVisible(false);
+		
+		if (event.getSource() == choiceNew) {
+			newPassword.setVisible(true);
+			
+		}
+		
+		if(event.getSource() == save) {
+			Log.info("New Password Saved!");
+			newPassword.setVisible(false);
+		}
+		
+	}
+	
 }
