cbfebdae2c4cb463dd9503ae58d6226250e7a40f
Dennis Kupec <denniskupec@gmail.com>
2019-11-12 14:36:41 -0600

    Added documentation to Authenticator and removed extra call to login() method

--- src/main/java/passmanager/controller/LoginController.java
+++ src/main/java/passmanager/controller/LoginController.java
@@ -14,10 +14,15 @@ public class LoginController extends BaseController {
 	@FXML private PasswordField password;
 	@FXML private Label errorMsg;
 	
+	/**
+	 * Routine for when the unlock button is pressed. It tries to authenticate the user, then change the scene.
+	 * @param event
+	 */
 	@FXML
 	public void doLogin(Event event) {
 		errorMsg.setVisible(false);
 		
+		// password field must have something entered
 		if (password.getText().isEmpty()) {
 			password.setText("");
 			errorMsg.setText("Enter your password to unlock.");
@@ -25,17 +30,18 @@ public class LoginController extends BaseController {
 			return;
 		}
 		
+		// attempt to login, or show an error on failure
 		Authenticator auth = new Authenticator(password.getText());
 		if (!auth.login()) {
 			password.setText("");
 			errorMsg.setText("Invalid password. Try again.");
 			errorMsg.setVisible(true);
+			return;
 		}
-
-		if (auth.login()) {
-			Log.info("Authenticated!");
 		
-			loadScene("/layout/listview.fxml");
+		// only runs if login was successful
+		Log.info("Authenticated!");
+		loadScene("/layout/listview.fxml");
 		}
 	}
 	
