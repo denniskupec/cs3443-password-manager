6dd309da178d271dd990b69e912519f8a5c4ced6
Dennis Kupec <denniskupec@gmail.com>
2019-11-19 08:16:11 -0600

    WIP: trying new styles for the list view

--- src/main/java/passmanager/controller/ListController.java
+++ src/main/java/passmanager/controller/ListController.java
@@ -1,114 +1,19 @@
 package passmanager.controller;
 
-import java.io.IOException;
-import java.net.URL;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ResourceBundle;
 import java.util.logging.Logger;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.*;
 import javafx.fxml.FXML;
-
-import javafx.fxml.Initializable;
-
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
-import passmanager.Database;
-import passmanager.Util;
-import passmanager.accountGettersSetters;
+import javafx.scene.image.ImageView;
+import passmanager.*;
 
-public class ListController extends BaseController implements Initializable {
 
+public class ListController extends BaseController {
 	private final static Logger Log = Util.getLogger(ListController.class);
-	@FXML MenuItem choiceNew;
-	@FXML TextField DisplayWebsite;
-	@FXML TextField DisplayUsername;
-	@FXML TextField DisplayPassword;
-	@FXML PasswordField hiddenPassword;
-	@FXML TextArea DisplayNotes;
-	@FXML MenuItem choiceSettings;
-	@FXML MenuItem choiceQuit;
-	@FXML Button DisplaySave;
-	@FXML CheckBox edit;
-	@FXML private TableView<accountGettersSetters> tableView;
-	@FXML private TableColumn<accountGettersSetters, String> account;
-	private ObservableList<accountGettersSetters> data;
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		try {
-			loadData();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	public void loadData() throws SQLException {
-		String sql = "SELECT * FROM entries";
-		Connection conn = Database.connect();
-		data = FXCollections.observableArrayList();
-		ResultSet rs = conn.createStatement().executeQuery(sql);
-		while (rs.next())
-		{
-			data.add(new accountGettersSetters(rs.getString("title"),rs.getString("username"),rs.getString("password"),rs.getString("url"),rs.getString("note")));
-		}
-		account.setCellValueFactory(new PropertyValueFactory<accountGettersSetters, String>("title"));
-		tableView.setItems(data);
-		rs.close();
-		conn.close();
-	}
-	@FXML
-	public void reload()
-	{
-		try {
-			loadData();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	/**
-	 * used to process the different choices in the menu bar
-	 * @param event
-	 */
-	public void onMenu(Event event) throws IOException {
-		// i don't think this is strictly necessary, but i just copied
-		// what dennis did to be safe
-		if (event.getSource() == choiceNew) {
-			loadNewEntry("/layout/newEntries.fxml");
-			Log.info("New Selected, inputting new entry");
-		}
-		if (event.getSource() == choiceSettings) {
-			loadNewEntry("/layout/settings.fxml");
-			Log.info("Settings selected");
-		}
-
-		if (event.getSource() == choiceQuit) {
-			stage.close();
-		}
-	}
 	
-	public void onEditEntry(Event event) {
-		if(event.getSource() == edit) {
-			if(edit.isSelected()) {
-				DisplayWebsite.setEditable(true);
-				DisplayUsername.setEditable(true);
-				DisplayPassword.setEditable(true);
-				DisplayPassword.setVisible(true);
-				hiddenPassword.setVisible(false);
-				DisplayNotes.setEditable(true);
-				DisplaySave.setVisible(true);
-			}
-			else {
-				DisplayWebsite.setEditable(false);
-				DisplayUsername.setEditable(false);
-				DisplayPassword.setEditable(false);
-				DisplayPassword.setVisible(false);
-				hiddenPassword.setVisible(true);
-				DisplayNotes.setEditable(false);
-				DisplaySave.setVisible(false);
-			}
-		}
-	}
+	@FXML Label statusMessage;
+	@FXML Button btnSearch;
+	@FXML TextField textfieldSearch;
+	@FXML Button btnEdit;
+	@FXML ImageView favicon;
+	@FXML Label title;
 }
